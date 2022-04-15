aca0da7a1d81b9c0b78728a55a45f76d3382802e
Clem-svg <clementine.fouque@gmail.com>
2022-04-15 15:06:24 +0200

    corrections and clean up

--- test_app.py
+++ test_app.py
@@ -3,21 +3,37 @@ import pytest
 from flask import Flask
 
 @pytest.mark.CheckRequest
-@pytest.mark.SuccessfulTest
+@pytest.mark.Successful
 @pytest.mark.parametrize("route", ["/", "/other"])
-def test_route_exists(client: Flask, route: str):
+def test_if_route_exists(client: Flask, route: str):
     response = client.get(route)
     assert response.status_code == 200
 
 @pytest.mark.CheckRequest
-@pytest.mark.FailedTest
+@pytest.mark.Failed
 @pytest.mark.parametrize("route", [783, "522", "/whatsup"])
-def test_route_do_not_exists(client: Flask, route):
+def test_if_route_do_not_exists(client: Flask, route):
     response = client.get(str(route))
     assert response.status_code == 404
     assert not "Hello".encode() in response.data
 
+@pytest.mark.CheckRequest
+@pytest.mark.Successful
+@pytest.mark.parametrize("page", [1, -48, 13])
+def test_content_other_page(client, page: int):
+    page = str(page)
+    response = client.get("/other?page=" + page)
+    assert response.status_code == 200
 
+@pytest.mark.Successful
+@pytest.mark.parametrize("value", [5, 45, 120])
+def test_if_exp_page_found(client, value: int):
+    receivedResponse = client.get("/exp?value=" + str(value))
+    expectedResponse = "Exposant 2 de " + str(value) + " : " + str(pow(value, 2))
+    assert expectedResponse() in receivedResponse.data
 
-
-
+@pytest.mark.Failed
+@pytest.mark.parametrize("value", [62, -4, "helloWorld"])
+def test_exp_page_with_wrong_value_type(client, value: int):
+    with pytest.raises(ValueError):
+        client.get("/exp?value=" + str(value))
--- test_homepage.py
+++ test_homepage.py
@@ -2,15 +2,14 @@ from conftest import client
 import pytest
 from flask import Flask
 
-@pytest.mark.SuccessfulTest
+@pytest.mark.Success
 def test_homepage(client: Flask):
     response = client.get("/")
     assert b"Hello, World" in response.data
 
 @pytest.mark.Mock
-@pytest.mark.SuccessfulTest
 def test_mocked_homepage(mocker):
-    def helloWorld():
-        return "<p>Hello, World</p>"
-    mocker.patch('test_homepage.test_homepage', helloWorld )
-    assert "Hello, World" in test_homepage()
+    def greet():
+        return "<p>Hello, love</p>"
+    mocker.patch('test_homepage.test_homepage', greet )
+    assert "Hello, love" in test_homepage()
